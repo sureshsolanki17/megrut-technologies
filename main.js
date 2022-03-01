45d08474c00ad3e7b01b0c35af4f94cba1b4aaef
sureshsolanki17 <93282222+sureshsolanki17@users.noreply.github.com>
2022-03-01 11:52:31 +0530

    my second commit

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 window.addEventListener("scroll", () => {
     let navbarStk = document.getElementById("sticky-nav");
     let naavButton = document.getElementById("js-nav-button");
-    if (window.pageYOffset >= 120) {
+    if (window.pageYOffset >= 80) {
         navbarStk.classList.add("sticky-nv");
         naavButton.classList.add("new-nav-btn");
-    } if (window.pageYOffset <= 120) {
+    } if (window.pageYOffset <= 80) {
         navbarStk.classList.remove("sticky-nv");
         naavButton.classList.remove("new-nav-btn");
     }
@@ -61,26 +61,54 @@ window.addEventListener("scroll", () => {
 });
 /* end sroll up */
 /* for carosol */
-let flag = 0;
-function controler(x){
-    flag = x ;
-    slideshow(flag);
-};
-slideshow(flag);
-function slideshow(){
-    let sleid = document.getElementsByClassName("carousel-contein");
-    for (let y of sleid) {
-        y.style.display = "none";
-    }
-    sleid[flag].style.display = "block";
-}
+// let flag = 0;
+// function controler(x){
+//     flag = x ;
+//     slideshow(flag);
+// };
+// slideshow(flag);
+// function slideshow(){
+//     let sleid = document.getElementsByClassName("carousel-contein");
+//     for (let y of sleid) {
+//         y.style.display = "none";
+//     }
+//     // sleid[flag].style.display = "block";
+// }
 // -------  Project-section
-function displayFunc1() {
-    document.getElementById("btnon1").classList.toggle("Project-link")
-}
-function displayFunc2() {
-    document.getElementById("btnon2").classList.toggle("Project-link")
-}
-function displayFunc3() {
-    document.getElementById("btnon3").classList.toggle("Project-link")
-}
+let allbrand = document.getElementById("btnon1");
+let branding = document.getElementById("btnon2");
+let development = document.getElementById("btnon3");
+let selectAllCard = document.querySelectorAll(".pro-active")
+    //  for All brand 
+allbrand.addEventListener("click", () => {
+    allbrand.classList.add("bottom-btn-line");
+    branding.classList.remove("bottom-btn-line");
+    development.classList.remove("bottom-btn-line");
+    
+    for (let y of selectAllCard) {
+        y.classList.remove("brabd_block");
+        y.classList.remove("dev_none");
+    }    
+});
+    //  for branding
+branding.addEventListener("click", () => {
+    branding.classList.add("bottom-btn-line");
+    allbrand.classList.remove("bottom-btn-line");
+    development.classList.remove("bottom-btn-line");
+
+    for (let y of selectAllCard) {
+        y.classList.remove("dev_none");
+        y.classList.add("brabd_block");
+    };    
+});
+    //  for Development 
+development.addEventListener("click", () => {
+    allbrand.classList.remove("bottom-btn-line");
+    branding.classList.remove("bottom-btn-line");
+    development.classList.add("bottom-btn-line");
+
+    for (let y of selectAllCard) {
+        y.classList.add("dev_none");
+    };    
+});      
+
